7ec31303db813d4e13b2a2d5eeb7a21b001e1085
iamsamhhh <sc_samchen@icloud.com>
2021-08-20 22:05:04 +0800

    Initial Commit

---
+++ SEngine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SEngine
+//
+//  Created by henry on 2021/8/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
